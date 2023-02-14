27a44304405d805b14ba7d220da156ae23a266a4
miftahurrahmi <miftahurrahmi45@gmail.com>
2023-02-14 09:47:43 +0700

    commit

--- src/Controllers/SnapController.php
+++ src/Controllers/SnapController.php
@@ -27,11 +27,11 @@ class SnapController extends Controller
     public function getSnapToken(Request $request)
     {
         try {
+
             $request->validate([
                 'id' => 'required|exists:Uasoft\Badaso\Module\Commerce\Models\Order,id',
                 'payment_type' => 'required|string|in:credit_card,gopay,cimb_clicks,bca_klikbca,bca_klikpay,bri_epay,telkomsel_cash,echannel,permata_va,other_va,bca_va,bni_va,bri_va,indomaret,alfamart,danamon_online,akulaku,shopeepay'
             ]);
-
             Config::$serverKey = env('MIDTRANS_SERVER_KEY');
             Config::$isProduction = env('APP_ENV') === 'production';
             Config::$isSanitized = true;
